5791946af7f7c8e43e5861f1985837bc5a05abcd
Gaby6666 <gabrielli.silva4@estudante.ifms.edu.br>
2025-01-22 15:25:25 -0400

    atualização

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Seja bem vindo</h1>
+<h1>sinta-se a vontade</h1>
